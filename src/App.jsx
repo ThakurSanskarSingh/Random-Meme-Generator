ea3d9ea584283778f6fb5acb183306f3037695a8
Sanskar Singh <sanskarsinghgamerz01@gmail.com>
2024-01-11 22:41:12 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/header'
+import Meme from './components/meme'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+    <Meme />
+   
+    </>
+  )
+}
+
+export default App
+
